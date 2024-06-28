dcddcb6b0773431ad59e6ca892e8ef18fd02d378
Marcus Parker <marcus.parker@limbsandthings.com>
2024-06-28 12:15:03 +0100

    try to directly connect to hosted db

--- app/__init__.py
+++ app/__init__.py
@@ -15,10 +15,10 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")  # local
 else:
-    uri = os.environ.get("DATABASE_URL")
-    if uri is not None:
-        if uri.startswith("postgres://"):
-            uri = uri.replace("postgres://", "postgresql://", 1)
+    # uri = os.environ.get("DATABASE_URL")
+    # if uri is not None:
+    #     if uri.startswith("postgres://"):
+    #         uri = uri.replace("postgres://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")  # heroku
 
 db = SQLAlchemy(app)
